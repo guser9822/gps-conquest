46e46b4e0dba03b0d34bd53b35b002344ed75eff
guser9822 <riccardo.daniello13@gmail.com>
2018-02-22 22:18:18 +0100

    Added an example of a spawn of the avator in the scene

--- Assets/GPConquest/Scripts/Client/UMASelectionController.cs
+++ Assets/GPConquest/Scripts/Client/UMASelectionController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 using TC.GPConquest.Player;
 using TC.Common;
 using UMA;
+using TC.UM4GPConquest.Utility;
 
 public class UMASelectionController : MonoBehaviour {
 
     public AssetLoaderController AssetLoaderController;
+    private UMADynamicAvatar UMADynamicAvator;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
+        UMADynamicAvator = GetComponent<UMADynamicAvatar>();
+    }
+
+    // Use this for initialization
+    void Start () {
         AssetLoaderController.CacheAllUMA(CommonNames.assetsNameStreamingFolder);
         Dictionary<string,UMATextRecipe> umasMap = AssetLoaderController.umaCharactersTemplates;
+
+        GameObject g = UMAGenericHelper.createUMAAvator(AssetLoaderController,
+            "HumanFemale 3", UMADynamicAvator,null);
+
 	}
 	
 	// Update is called once per frame
---
+++ Assets/UM4GPConquest/Script/UMAGenericHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TC.GPConquest.Player;
+using UMA;
+using System;
+
+namespace TC.UM4GPConquest.Utility
+{
+    public static class UMAGenericHelper
+    {
+        /**
+         * Given an AssetLoaderController in input,a UMA avator's name and a UMADynamicAvator
+         * create and spawn a new uma avator. It is possible to pass also a callback
+         * that it's executed when the uma avator is created.
+         * */
+        public static GameObject createUMAAvator(AssetLoaderController assetLoaderController,
+            string umaAvatorName,
+            UMADynamicAvatar umaDynamicAvator,
+            Action<UMAData> onCharacterCreatedCallback)
+        {
+            if (assetLoaderController != null && umaAvatorName != null)
+            {
+                //Set/spawn a UMA Avator
+                GameObject thisUma = umaDynamicAvator.gameObject;
+                UMADynamicAvatar thisUmaDynamicAvator = thisUma.GetComponent<UMADynamicAvatar>();
+
+                thisUmaDynamicAvator.context = assetLoaderController.context;
+                thisUmaDynamicAvator.umaGenerator = assetLoaderController.generator;
+                thisUmaDynamicAvator.loadOnStart = false;
+                thisUmaDynamicAvator.Initialize();
+                thisUmaDynamicAvator.animationController = assetLoaderController.thirdPersonController;
+                UMATextRecipe recipe = assetLoaderController.umaCharactersTemplates[umaAvatorName];
+                thisUmaDynamicAvator.Load(recipe);
+
+                if(onCharacterCreatedCallback!=null)
+                    thisUma.GetComponent<UMAData>().OnCharacterCreated += onCharacterCreatedCallback;
+            }
+            else
+            {
+                if(assetLoaderController==null)
+                    throw new System.ArgumentException("AssetLoader given in input is null");
+                else if (umaAvatorName == null)
+                    throw new System.ArgumentException("UMA's avator name given in input is null");
+            }
+            return new GameObject();
+        }
+    }
+}
+
