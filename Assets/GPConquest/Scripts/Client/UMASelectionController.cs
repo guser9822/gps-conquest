1f85a08db5c40f5714f01d677c87cc51ea357c3f
guser9822 <riccardo.daniello13@gmail.com>
2018-02-22 07:21:05 +0100

    Finished setup of the SelectionUMA scene

---
+++ Assets/GPConquest/Scripts/Client/UMASelectionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TC.GPConquest.Player;
+using TC.Common;
+
+public class UMASelectionController : MonoBehaviour {
+
+    public AssetLoaderController AssetLoaderController;
+
+	// Use this for initialization
+	void Start () {
+        AssetLoaderController.CacheAllUMA(CommonNames.assetsNameStreamingFolder);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
