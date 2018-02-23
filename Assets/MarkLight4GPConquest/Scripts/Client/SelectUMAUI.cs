78c44668caf8311803f021c8cd957b2609500769
guser9822 <riccardo.daniello13@gmail.com>
2018-02-23 07:27:14 +0100

    Added next and prev button for selecting the character

---
+++ Assets/MarkLight4GPConquest/Scripts/Client/SelectUMAUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MarkLight.Views.UI;
+
+
+namespace TC.GPConquest.MarkLight4GPConquest
+{
+    public class SelectUMAUI : UIView
+    {
+
+        public void NextUMA()
+        {
+
+        }
+
+        public void PrevUMA()
+        {
+
+        }
+
+    }
+
+}
