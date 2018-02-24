c15b8e4b29962b71e93cabf1d2246cafafca9eb0
guser9822 <riccardo.daniello13@gmail.com>
2018-02-24 20:03:54 +0100

    Moved a lot of function from selection controller to UMAGenericHelper

--- Assets/GPConquest/Scripts/Client/UMASelectionController.cs
+++ Assets/GPConquest/Scripts/Client/UMASelectionController.cs
@@ -26,79 +26,15 @@ public class UMASelectionController : MonoBehaviour {
     void Start () {
         AssetLoaderController.CacheAllUMA(CommonNames.assetsNameStreamingFolder);
         //Dictionary<string, UMATextRecipe> umasMap = AssetLoaderController.umaCharactersTemplates;
-        createdUmas = SpawnAllAvator(createdUmas,
+        createdUmas = UMAGenericHelper.SpawnAllAvator(createdUmas,
             AssetLoaderController,
             CommonNames.avators,
             transform,
             Vector3.zero,
             Quaternion.Euler(0,180,0));
 
-        ToggleUmasActivation(createdUmas,6);
+        UMAGenericHelper.ToggleUmasActivation(createdUmas);
     }
 
-    /*
-     * Given a non empty array of GameObject and an AssetLoaderController, spawn all the avators available
-     * and uses the array to get a reference to them. It is possibile to set rotation,position and a parent.
-     * Umas are generated according the array of names given in input.
-     * */
-    private GameObject[] SpawnAllAvator(GameObject[] umasSet,
-        AssetLoaderController _assetLoaderController,
-        string[] arrayOfNames,
-        Transform _parent,
-        Vector3 umasPosition,
-        Quaternion umasRotation
-        )
-    {
-        for (int x = 0; x < umasSet.Length; x++)
-        {
-            umasSet[x] = new GameObject();
-            umasSet[x].gameObject.AddComponent<UMADynamicAvatar>();
-            umasSet[x].gameObject.name = arrayOfNames[x];
-            umasSet[x] = UMAGenericHelper.createUMAAvator(_assetLoaderController,
-                arrayOfNames[x],
-                umasSet[x].gameObject.GetComponent<UMADynamicAvatar>(),
-                null);
-            umasSet[x].transform.parent = _parent;
-            umasSet[x].transform.position = umasPosition;
-            umasSet[x].transform.rotation= umasRotation;
-        }
-        return umasSet;
-    }
-
-    /* 
-     * Deactivates all the uma's except the one that correspond to the index
-     * given in input. If the index parameter doesn't get valorized, it means 
-     * that all the uma's will be deactivated.
-     * */
-    public GameObject ToggleUmasActivation(GameObject[] umasSet, int _toActivate = int.MaxValue)
-    {
-        GameObject result = null;
-        if (IsIndexInUmas(umasSet, _toActivate))
-        {
-            result = umasSet[_toActivate];
-            _ToggleUmasActivationInternalLogic(umasSet, x =>
-                {
-                    if (IsWantedUma(x, umasSet[_toActivate]))
-                        umasSet[_toActivate].gameObject.SetActive(true);
-                    else x.SetActive(false);
-                });
-        }
-        else _ToggleUmasActivationInternalLogic(umasSet, x => x.SetActive(false));
-        return result;
-    }
-
-    private void _ToggleUmasActivationInternalLogic(GameObject[] umasSet,Action<GameObject> _action)
-    {
-        Array.ForEach(umasSet,_action);
-    }
-
-    private bool IsIndexInUmas(GameObject[] umas, int index)
-    {
-        return Enumerable.Range(0, umas.Length).Contains<int>(index);
-    }
-
-    private bool IsWantedUma(GameObject _wantedUma,GameObject _maybe)
-    {
-        return _maybe.Equals(_wantedUma);
-    }
+    
 }
--- Assets/UM4GPConquest/Script/UMAGenericHelper.cs
+++ Assets/UM4GPConquest/Script/UMAGenericHelper.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TC.GPConquest.Player;
 using UMA;
 using System;
+using System.Linq;
 
 namespace TC.UM4GPConquest.Utility
 {
@@ -52,6 +53,71 @@ namespace TC.UM4GPConquest.Utility
             return thisUma;
         }
 
+        /*
+     * Given a non empty array of GameObject and an AssetLoaderController, spawn all the avators available
+     * and uses the array to get a reference to them. It is possibile to set rotation,position and a parent.
+     * Umas are generated according the array of names given in input.
+     * */
+        public static GameObject[] SpawnAllAvator(GameObject[] umasSet,
+            AssetLoaderController _assetLoaderController,
+            string[] arrayOfNames,
+            Transform _parent,
+            Vector3 umasPosition,
+            Quaternion umasRotation
+            )
+        {
+            for (int x = 0; x < umasSet.Length; x++)
+            {
+                umasSet[x] = new GameObject();
+                umasSet[x].gameObject.AddComponent<UMADynamicAvatar>();
+                umasSet[x].gameObject.name = arrayOfNames[x];
+                umasSet[x] = UMAGenericHelper.createUMAAvator(_assetLoaderController,
+                    arrayOfNames[x],
+                    umasSet[x].gameObject.GetComponent<UMADynamicAvatar>(),
+                    null);
+                umasSet[x].transform.parent = _parent;
+                umasSet[x].transform.position = umasPosition;
+                umasSet[x].transform.rotation = umasRotation;
+            }
+            return umasSet;
+        }
+
+        /* 
+         * Deactivates all the uma's except the one that correspond to the index
+         * given in input. If the index parameter doesn't get valorized, it means 
+         * that all the uma's will be deactivated.
+         * */
+        public static GameObject ToggleUmasActivation(GameObject[] umasSet, int _toActivate = int.MaxValue)
+        {
+            GameObject result = null;
+            if (IsIndexInUmas(umasSet, _toActivate))
+            {
+                result = umasSet[_toActivate];
+                _ToggleUmasActivationInternalLogic(umasSet, x =>
+                {
+                    if (IsWantedUma(x, umasSet[_toActivate]))
+                        umasSet[_toActivate].gameObject.SetActive(true);
+                    else x.SetActive(false);
+                });
+            }
+            else _ToggleUmasActivationInternalLogic(umasSet, x => x.SetActive(false));
+            return result;
+        }
+
+        private static void _ToggleUmasActivationInternalLogic(GameObject[] umasSet, Action<GameObject> _action)
+        {
+            Array.ForEach(umasSet, _action);
+        }
+
+        private static bool IsIndexInUmas(GameObject[] umas, int index)
+        {
+            return Enumerable.Range(0, umas.Length).Contains<int>(index);
+        }
+
+        private static bool IsWantedUma(GameObject _wantedUma, GameObject _maybe)
+        {
+            return _maybe.Equals(_wantedUma);
+        }
 
     }
 }
