056aa8da851b527278eed4d1f85a8e1b36c7905a
guser9822 <riccardo.daniello13@gmail.com>
2018-02-25 12:16:29 +0100

    Added Client Menu and Register scenes/views. To re-arrange all the scenes

---
+++ Assets/Common/Scripts/GPCSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TC.Common
+{
+    public static class GPCSceneManager
+    {
+        private enum GPCSceneContainer
+        {
+            CLIENT_MENU,
+            REGISTER,
+            SELECT_UMA,
+            LOGIN,
+            GAME_SCENE,
+            OPTION,
+        }
+
+
+
+    }
+
+}
---
+++ Assets/MarkLight4GPConquest/Scripts/Client/ClientMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using MarkLight.Views.UI;
+using UnityEngine.SceneManagement;
+
+namespace TC.GPConquest.MarkLight4GPConquest
+{
+    public class ClientMenuUI : UIView
+    {
+        public void CallRegister()
+        {
+
+        }
+
+        public void CallLogin()
+        {
+            SceneManager.LoadScene(/*SceneManager.GetActiveScene().buildIndex + 1*/3);
+        }
+
+        public void CallOption()
+        {
+
+        }
+
+        public void CallExit()
+        {
+            Application.Quit();
+        }
+    }
+}
+
