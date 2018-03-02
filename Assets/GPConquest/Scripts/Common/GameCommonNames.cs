c8b0b4130c641185861418d0230da5b904319669
guser9822 <riccardo.daniello13@gmail.com>
2018-03-02 22:46:37 +0100

    Added Common Class for manage Faction and common game Names

---
+++ Assets/GPConquest/Scripts/Common/GameCommonNames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TC.GPConquest.Common {
+
+    public static class GameCommonNames
+    {
+        public static readonly string RED_FACTION = "RED_FACTION";
+        public static readonly string GREEN_FACTION = "GREEN_FACTION";
+    }
+
+}
