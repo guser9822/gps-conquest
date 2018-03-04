457c3290a51937ce1787575c3630b164c19549a0
guser9822 <riccardo.daniello13@gmail.com>
2018-03-04 12:14:58 +0100

    Added default Faction in the selection scene

--- Assets/GPConquest/Scripts/Common/GameCommonNames.cs
+++ Assets/GPConquest/Scripts/Common/GameCommonNames.cs
@@ -7,8 +7,9 @@ namespace TC.GPConquest.Common {
 
     public static class GameCommonNames
     {
-        public static readonly string RED_FACTION = "RED_FACTION";
-        public static readonly string GREEN_FACTION = "GREEN_FACTION";
+        public static readonly string RED_FACTION = "RED";
+        public static readonly string GREEN_FACTION = "GREEN";
+        public static readonly List<string> FACTIONS = new List<string>(){ RED_FACTION,GREEN_FACTION}; 
     }
 
 }
--- Assets/MarkLight4GPConquest/Scripts/Client/SelectUMAUI.cs
+++ Assets/MarkLight4GPConquest/Scripts/Client/SelectUMAUI.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using MarkLight.Views.UI;
 using TC.GPConquest.Player;
+using MarkLight;
+using TC.GPConquest.Common;
 
 namespace TC.GPConquest.MarkLight4GPConquest
 {
@@ -17,16 +19,45 @@ namespace TC.GPConquest.MarkLight4GPConquest
          * oblivion, and then, you lose any reference to it.
          * 
          * I think that, so, it's better to let MarkLightUI handles all the 
-         * scripts for you, just declaring them as class's properties.cThis 
+         * scripts for you, just declaring them as class's properties. This 
          * will also make more simple every dipendecy problem and will also 
          * avoid use of the methods like GameObject.FindObjectOfType<>().
          * */
         public UMASelectionController UmaSelectionController;
         public AssetLoaderController AssetLoaderController;
+        public ObservableList<string> FactionsList;
+        public ComboBox ComboBoxFactions;
+        [MapTo("FactionsList.SelectedItem")]
+        public _object XFaction;
 
+        /*
+         * In order to see a default faction in the selection scene
+         * we create an ObservableList overriding Initialize method of 
+         * the MarkLightUI framework.
+         * **/
         public override void Initialize()
         {
             base.Initialize();
+            FactionsList = InitializeFactionsList(GameCommonNames.FACTIONS);
+        }
+
+        protected ObservableList<string> InitializeFactionsList(List<string> _factions)
+        {
+            var factionList = new ObservableList<string>();
+            _factions.ForEach(x => factionList.Add(x));
+            return factionList;
+        }
+
+        /*
+         * After the ObservableList is created we choose here at which index
+         * should reference, or rather, which faction should be picked and
+         * showed in the selection scene. This operation doesn't work in the
+         * Initialize method (the overrided one) called above.
+         * **/
+        private void Start()
+        {
+            FactionsList.SelectedIndex = 1;
+            XFaction = ComboBoxFactions.SelectedItem;
         }
 
         public void NextUMA()
