b0e9545e45371b74a3227fed00eccd906f93eb8f
guser9822 <riccardo.daniello13@gmail.com>
2018-07-03 23:11:48 +0200

    Added tower entity graphic ui prefab

---
+++ Assets/MarkLight4GPConquest/Scripts/Common/TowerEntityUI.cs
@@ -0,0 +1,42 @@
+﻿using MarkLight;
+using MarkLight.Views.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TC.GPConquest.MarkLight4GPConquest.Common
+{
+    /// <summary>
+    /// This class manage the graphic interface of tower entities
+    /// </summary>
+    public class TowerEntityUI : UIView
+    {
+        public _string TowerFaction;
+        public Window TowerUIWindow;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            TowerFaction.Value = "TEST";
+        }
+
+        public void Start()
+        {
+            ToggleWindow();
+        }
+
+        public void ToggleWindow()
+        {
+            if (TowerUIWindow.IsOpen)
+            {
+                TowerUIWindow.Close();
+            }
+            else
+            {
+                TowerUIWindow.Open();
+            }
+        }
+
+
+    }
+}
